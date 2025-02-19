7d6f938f7b83fa256079a6199644d1c15494e6be
Bryan Lindberg <bryanlindberg331@gmail.com>
2025-02-19 14:13:44 -0500

    2/19/25

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageViewPlaceholder: UIImageView! //hidden image to be used as placeholder for swap
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
 
+    }
+    
+    @IBAction func maxRollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceSix")
+    }
+    
+    @IBAction func switcharooButtonPressed(_ sender: UIButton) {
+        diceImageViewPlaceholder.image = diceImageView1.image
+        diceImageView1.image = diceImageView2.image
+        diceImageView2.image = diceImageViewPlaceholder.image
+    }
+    
+    
 }
 
